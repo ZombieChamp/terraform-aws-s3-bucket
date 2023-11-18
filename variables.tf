77acbac0533460aa47d5b911333c26e06a5a897b
Matthew Mawson <matthewmawson@gmail.com>
2023-11-18 13:08:22 +0000

    validation for bucket name

--- variables.tf
+++ variables.tf
@@ -2,6 +2,51 @@ variable "bucket" {
   default     = null
   type        = string
   description = "(Optional, Forces new resource) The name of the bucket to create. If omitted, Terraform will assign a random, unique name."
+
+  validation {
+    condition     = var.bucket == null ? true : length(var.bucket) >= 3 && length(var.bucket) <= 63
+    error_message = "Bucket names must be between 3 (min) and 63 (max) characters long."
+  }
+
+  validation {
+    condition     = var.bucket == null ? true : can(regex("^[a-z0-9.-]*$", var.bucket))
+    error_message = "Bucket names can consist only of lowercase letters, numbers, dots (.), and hyphens (-)."
+  }
+
+  validation {
+    condition     = var.bucket == null ? true : can(regex("^[a-z0-9].*[a-z0-9]$", var.bucket))
+    error_message = "Bucket names must begin and end with a letter or number."
+  }
+
+  validation {
+    condition     = !can(regex(".*[.][.].*", var.bucket))
+    error_message = "Bucket names must not contain two adjacent periods."
+  }
+
+  validation {
+    condition     = !can(cidrnetmask("${var.bucket}/32"))
+    error_message = "Bucket names must not be formatted as an IP address (for example, 192.168.5.4)."
+  }
+
+  validation {
+    condition     = var.bucket == null ? true : !startswith(var.bucket, "xn--")
+    error_message = "Bucket names must not start with the prefix xn--."
+  }
+
+  validation {
+    condition     = var.bucket == null ? true : !startswith(var.bucket, "sthree-")
+    error_message = "Bucket names must not start with the prefix sthree-."
+  }
+
+  validation {
+    condition     = var.bucket == null ? true : !endswith(var.bucket, "-s3alias")
+    error_message = "Bucket names must not end with the suffix -s3alias. This suffix is reserved for access point alias names. For more information, see Using a bucket-style alias for your S3 bucket access point (https://docs.aws.amazon.com/AmazonS3/latest/userguide/access-points-alias.html)."
+  }
+
+  validation {
+    condition     = var.bucket == null ? true : !endswith(var.bucket, "--ol-s3")
+    error_message = "Bucket names must not end with the suffix --ol-s3. This suffix is reserved for Object Lambda Access Point alias names. For more information, see How to use a bucket-style alias for your S3 bucket Object Lambda Access Point (https://docs.aws.amazon.com/AmazonS3/latest/userguide/olap-use.html#ol-access-points-alias)."
+  }
 }
 
 variable "bucket_prefix" {
