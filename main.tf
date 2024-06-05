40eda86bcffd08b0e2d5245a1fef43b9848ecf1b
Matthew Mawson <matthewmawson@gmail.com>
2024-06-05 21:17:51 +0100

    minor formatting

--- main.tf
+++ main.tf
@@ -9,7 +9,8 @@ resource "aws_s3_bucket" "this" {
   bucket_prefix       = var.bucket_prefix
   force_destroy       = var.force_destroy
   object_lock_enabled = var.object_lock_enabled
-  tags                = var.tags
+
+  tags = var.tags
 }
 
 resource "aws_s3_bucket_public_access_block" "this" {
